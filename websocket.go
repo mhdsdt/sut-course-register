3fa5de0e3aff9f039a4ddcb1b3ebc2817fe73f7f
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2023-09-11 22:36:54 +0330

    Move functions relate to websocket to websocket.go

---
+++ websocket.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+func handleMessage(message []byte) {
+	var msg struct {
+		Type string `json:"type"`
+	}
+
+	if err := json.Unmarshal(message, &msg); err != nil {
+		fmt.Printf("Error parsing WebSocket message: %v\n", err)
+		return
+	}
+
+	switch msg.Type {
+	case "userState":
+		handleUserState(message)
+	case "listUpdate":
+		handleListUpdate(message)
+		registerCourses()
+	default:
+		fmt.Printf("Received unknown message type: %s\n", msg.Type)
+	}
+}
+
+func handleUserState(message []byte) {
+	var parsedMessage UserStateMessage
+	err := json.Unmarshal(message, &parsedMessage)
+	if err != nil {
+		return
+	}
+
+	if len(favoriteCourses) == 0 {
+		favoriteCourses = parsedMessage.Message.Favorites
+	}
+
+	registrationTime = parsedMessage.Message.RegistrationTime
+	fmt.Printf("favoriteCourses: %v\n", favoriteCourses)
+}
+
+func handleListUpdate(message []byte) {
+	var parsedMessage ListUpdateMessage
+	if err := json.Unmarshal(message, &parsedMessage); err != nil {
+		fmt.Printf("Error parsing listUpdate message: %v\n", err)
+		return
+	}
+
+	coursesData = parsedMessage.Message
+	fmt.Println("Updated list of courses")
+
+	timeRemaining := time.Until(time.Unix(int64(registrationTime)/1000, 0))
+	if timeRemaining >= 0 {
+		fmt.Print("\rRegistration will start in ", formatDuration(timeRemaining))
+	}
+
+	if onTimeRegistration {
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+		for range ticker.C {
+			timeRemaining := time.Until(time.Unix(int64(registrationTime)/1000, 0))
+			fmt.Print("\rRegistration will start in ", formatDuration(timeRemaining))
+			if timeRemaining <= 0 {
+				break
+			}
+		}
+	}
+	fmt.Println()
+}
